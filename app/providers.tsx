b71f2c3fcbe2834bac1ca1228ed65125604c2449
xndadelin <adelinmihai071@gmail.com>
2025-10-26 13:56:20 +0200

    Cleanup & configure @tanstack/query & @mantine UI.

---
+++ app/providers.tsx
@@ -0,0 +1,17 @@
+'use client';
+import type { ReactNode } from "react";
+import { MantineProvider } from "@mantine/core";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+export function Providers({ children} : { children: ReactNode }) {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <MantineProvider>
+                {children}
+            </MantineProvider>
+        </QueryClientProvider>
+    )
+}
